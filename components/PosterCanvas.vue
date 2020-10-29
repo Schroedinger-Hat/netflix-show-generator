b83aba0c2dd8c12e55680b02d040e12ddb7b1501
thejoin95 <miki.lombi@gmail.com>
2020-10-29 23:29:17 +0100

    init canvas + fix some scss rules

---
+++ components/PosterCanvas.vue
@@ -0,0 +1,15 @@
+<template>
+  <section class="poster-canvas">
+    <h1>Canvas</h1>
+    <canvas id="main-canvas" />
+  </section>
+</template>
+<script>
+export default {
+  data () {
+    return {}
+  }
+}
+</script>
+<style scoped lang="scss">
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <nav
-      class="navbar header has-shadow is-primary"
+      class="navbar header is-primary"
       role="navigation"
       aria-label="main navigation"
     >
@@ -11,8 +11,8 @@
           href="/"
         >
           <img
-            src="~assets/buefy.png"
-            alt="Buefy"
+            src="~assets/logo.png"
+            alt="Schrodinger Hat"
             height="28"
           >
         </a>
@@ -24,51 +24,6 @@
         </div>
       </div>
     </nav>
-
-    <section class="main-content columns">
-      <aside class="column is-2 section">
-        <p class="menu-label is-hidden-touch">
-          General
-        </p>
-        <ul class="menu-list">
-          <li
-            v-for="(item, key) of items"
-            :key="key"
-          >
-            <nuxt-link
-              :to="item.to"
-              exact-active-class="is-active"
-            >
-              <b-icon :icon="item.icon" /> {{ item.title }}
-            </nuxt-link>
-          </li>
-        </ul>
-      </aside>
-
-      <div class="container column is-10">
-        <nuxt />
-      </div>
-    </section>
+    <nuxt />
   </div>
 </template>
-
-<script>
-export default {
-  data () {
-    return {
-      items: [
-        {
-          title: 'Home',
-          icon: 'home',
-          to: { name: 'index' }
-        },
-        {
-          title: 'Inspire',
-          icon: 'lightbulb',
-          to: { name: 'inspire' }
-        }
-      ]
-    }
-  }
-}
-</script>
