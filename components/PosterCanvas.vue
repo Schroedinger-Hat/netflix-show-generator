0d8e6b3694eae6d97b7ae514ded52ae2de2e7d41
thejoin95 <miki.lombi@gmail.com>
2020-11-08 21:40:56 +0100

    add v-title

--- components/PosterCanvas.vue
+++ components/PosterCanvas.vue
@@ -7,7 +7,32 @@
       @dragend="handleDragend"
     >
       <v-layer>
-        <v-text key="head-title" :config="{text: '<h1>Titolo</h1>'}" />
+        <v-text
+          key="head-title"
+          :config="{
+            id: 'head-title',
+            draggable: true,
+            x: 20,
+            y: 180,
+            text: 'Il re',
+            fontSize: 24,
+            fontFamily: 'HelveticaNeue',
+            fill: '#fff',
+          }"
+        />
+        <v-text
+          key="subtitle"
+          :config="{
+            id: 'subtitle',
+            draggable: true,
+            x: 20,
+            y: 230,
+            text: '2019 | VM14 | 2h 20min | Drammi',
+            fontSize: 14,
+            fontFamily: 'HelveticaNeue',
+            fill: '#a3a3a3',
+          }"
+        />
       </v-layer>
       <v-layer ref="layer">
         <v-star
@@ -56,7 +81,7 @@ export default {
     this.configKonva.width = window.innerWidth
     this.configKonva.height = window.innerHeight
 
-    for (let n = 0; n < 30; n++) {
+    for (let n = 0; n < 5; n++) {
       this.list.push({
         id: Math.round(Math.random() * 10000).toString(),
         x: Math.random() * this.configKonva.width,
@@ -82,6 +107,10 @@ export default {
     handleDragstart (e) {
       // save drag element:
       this.dragItemId = e.target.id()
+      if (this.dragItemId.match(/\d/) !== -1) {
+        return false
+      }
+
       // move current element to the top:
       const item = this.list.find(i => i.id === this.dragItemId)
       const index = this.list.indexOf(item)
